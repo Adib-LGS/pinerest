2e473966dd056d4d9f6dcd76b29ddaef19830838
Adib-Legastelois <adib.legastelois@icloud.com>
2020-09-08 18:05:41 +0100

    Use createFormBuilder()/Create a from to send pins + Using naming route names

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -1,25 +1,53 @@
 <?php
 
 namespace App\Controller;
-
-
+use App\Entity\Pin;
 use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PinsController extends AbstractController
 {
 
     /**
-     * @Route("/", name="")
+     * @Route("/", name="home", methods={"GET"})
      */
     public function index(PinRepository $repo): Response
     {
         //dd($repo->findAll());
-
         //$pins = $repo->findAll();
-
         return $this->render('pins/index.html.twig', ['pins' => $repo->findAll()]);
     }
+
+    /**
+     * @Route("/pins/create", name="pins.create", methods={"GET", "POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $pin = new Pin;
+
+        $form = $this->createFormBuilder($pin)
+        ->add('title', TextType::class, ['attr' => ['autofocus' => true]])
+        ->add('description', TextareaType::class, ['attr' => ['rows' => 10, 'cols' => 50]])
+        ->getForm();
+        
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            
+            $em->persist($pin);
+            $em->flush();
+
+            return $this->redirectToRoute('home');
+        }
+        //dd($form);
+        return $this->render('pins/create.html.twig', ['form' => $form->createView()]);
+    }
+
 }
